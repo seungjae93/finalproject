3255e6e4a03320a8528a21c5a27923dae276d28a
seungjae93 <lsj72023892@gmail.com>
2022-12-31 10:31:42 +0900

    :tada: finalproject 파일구조

---
+++ src/shared/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "../pages/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
