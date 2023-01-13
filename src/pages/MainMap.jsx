edb942a9ba0f3d99acb07ae60dfd3caa81d3fa88
seungjae93 <lsj72023892@gmail.com>
2023-01-13 09:34:12 +0900

    :sparkles: map 동서남북 좌표 서버 전송 확인

--- src/pages/MainMap.jsx
+++ src/pages/MainMap.jsx
@@ -21,11 +21,9 @@ const MainMap = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [searchAddress, setSearchAddress] = useState("");
   const [searchData, setSearchData] = useState([]);
-  const [map, setMap] = useState(); //지도
   const [pos, setPos] = useState(); //경도 위도
-  const [level, setLevel] = useState(); //지도 줌레벨
+  const [zoomLevel, setZoomLevel] = useState(); //지도 줌레벨
   const [info, setInfo] = useState(); //지도 레벨에 따른 위도 경도
-  const [click, setClick] = useState(0);
 
   const positions = [
     {
@@ -63,7 +61,9 @@ const MainMap = () => {
 
       const { data } = response.data;
       searchData(data);
-    } catch (error) {}
+    } catch (error) {
+      console.log("get에러를 잡았어", error);
+    }
   }, 500);
 
   //장소 검색 객체 생성
@@ -74,18 +74,9 @@ const MainMap = () => {
     if (status === kakao.maps.services.Status.OK) {
       const newSearch = data[0];
       setState({
-        center: { lat: newSearch.y, lng: newSearch.x },
+        center: { lat: Number(newSearch.y), lng: Number(newSearch.x) },
+        isPanto: true,
       });
-      // setMap({
-      //   swLatLng: {
-      //     lat: map.getBounds().getSouthWest().getLat(),
-      //     lng: map.getBounds().getSouthWest().getLng(),
-      //   },
-      //   neLatLng: {
-      //     lat: map.getBounds().getNorthEast().getLat(),
-      //     lng: map.getBounds().getNorthEast().getLng(),
-      //   },
-      // });
     }
   };
 
@@ -97,9 +88,9 @@ const MainMap = () => {
       await axios.post(`https://spart-instagram.shop/search`, {
         text: `${searchAddress}`,
       });
-      setClick(click + 1);
-      console.log("검색");
-    } catch (error) {}
+    } catch (error) {
+      console.log("post에러를 잡았어", error);
+    }
     setSearchAddress("");
   }, [searchAddress]);
 
@@ -112,43 +103,46 @@ const MainMap = () => {
       console.log("post에러를 잡았어", error);
     }
   };
+  const onDragHandler = async () => {
+    try {
+      await axios.post(`https://spart-instagram.shop/map`, {
+        ...info,
+        zoomLevel,
+      });
+    } catch (error) {
+      console.log("post에러를 잡았어", error);
+    }
+  };
 
-  // const handleMapInfo = () => {
-  //   {
-  //     map &&
-  //       setPos({
-  //         center: {
-  //           lat: map.getCenter().getLat(),
-  //           lng: map.getCenter().getLng(),
-  //         },
-  //         swLatLng: {
-  //           lat: map.getBounds().getSouthWest().getLat(),
-  //           lng: map.getBounds().getSouthWest().getLng(),
-  //         },
-  //         neLatLng: {
-  //           lat: map.getBounds().getNorthEast().getLat(),
-  //           lng: map.getBounds().getNorthEast().getLng(),
-  //         },
-  //       });
-  //   }
-  // };
-  const checkCenterChanged = () => {
-    console.log("center.....");
+  const onGeoHandler = async (geoLocation) => {
+    try {
+      await axios.post(`https://spart-instagram.shop/map`, {
+        ...geoLocation,
+        zoomLevel,
+      });
+    } catch (error) {
+      console.log("post에러를 잡았어", error);
+    }
   };
 
   useEffect(() => {
-    if (!map) return;
-    map.addEventListener("center_changed", checkCenterChanged);
-  }, []);
-
-  useEffect(() => {
-    console.log(mapRef.current);
+    /* 현재 보이는 위치에 대한 좌표 값을 받아와주는 부분 */
     const mapObject = mapRef.current;
     if (!mapObject) return;
-    console.log("useEffecc!t");
-    console.log(mapObject.getBounds().getSouthWest().getLat());
-  }, [mapRef, searchAddress, click]);
-  console.log(mapRef.current);
+    const geoLocation = {
+      swLatLng: {
+        lat: mapObject.getBounds().getSouthWest().getLat(),
+        lng: mapObject.getBounds().getSouthWest().getLng(),
+      },
+      neLatLng: {
+        lat: mapObject.getBounds().getNorthEast().getLat(),
+        lng: mapObject.getBounds().getNorthEast().getLng(),
+      },
+    };
+    onGeoHandler(geoLocation);
+    setPos(geoLocation);
+  }, [state]);
+  console.log(pos);
   return (
     <>
       {modalOpen && <TotalModal modalHandler={modalHandler} />}
@@ -171,10 +165,7 @@ const MainMap = () => {
         </SearchContainer>
         <StMapContainer>
           <Map // 지도를 표시할 Container
-            center={
-              // 지도의 중심좌표
-              state.center
-            }
+            center={state.center}
             isPanto={state.isPanto}
             style={{
               // 지도의 크기
@@ -183,11 +174,7 @@ const MainMap = () => {
             }}
             ref={mapRef}
             level={3} // 지도의 확대 레벨
-            onZoomChanged={(map) => setLevel(map.getLevel())}
-            onCreate={(map) => {
-              console.log("oncreate!!!!!!!!!");
-              setMap(map);
-            }}
+            onZoomChanged={(map) => setZoomLevel(map.getLevel())}
             onDragEnd={(map) => {
               setPos({
                 swLatLng: {
@@ -201,12 +188,20 @@ const MainMap = () => {
               });
               onPosHandler();
             }}
-            onBoundsChanged={(map) =>
+            onBoundsChanged={(map) => {
               setInfo({
-                sw: map.getBounds().getSouthWest().toString(),
-                ne: map.getBounds().getNorthEast().toString(),
-              })
-            }
+                swLatLng: {
+                  lat: map.getBounds().getSouthWest().getLat(),
+                  lng: map.getBounds().getSouthWest().getLng(),
+                },
+                neLatLng: {
+                  lat: map.getBounds().getNorthEast().getLat(),
+                  lng: map.getBounds().getNorthEast().getLng(),
+                },
+              });
+              setZoomLevel(map.getLevel());
+              onDragHandler();
+            }}
           >
             {positions.map((position) => {
               return (
