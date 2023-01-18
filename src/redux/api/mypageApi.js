d137e3cf0d2ac86c08346eb471f4ef0a9f0bc29c
heeseung lee <heeseung721@gmail.com>
2023-01-18 23:36:58 +0900

    :art:fix:Api 서버주소 수정

--- src/redux/api/mypageApi.js
+++ src/redux/api/mypageApi.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { getCookie } from "../../shared/cookie";
 import setToken from "../../shared/setToken";
 import { instance } from "./instance";
@@ -7,17 +6,18 @@ export const getmypageReviews = async () => {
   const accessToken = getCookie("token");
   setToken(accessToken);
   const headers = { authorization: `Bearer ${accessToken}` };
-
-  const response = await axios.get(
-    "https://spart-instagram.shop/review/myReview",
-    {
-      headers: headers,
-    }
-  );
+  const response = await instance.get("/review/myReview", {
+    headers: headers,
+  });
   return response.data;
 };
 
 export const getmypagePosts = async () => {
-  const response = await axios.get("https://goldpumpkin.shop/posts/me");
+  const accessToken = getCookie("token");
+  setToken(accessToken);
+  const headers = { authorization: `Bearer ${accessToken}` };
+  const response = await instance.get("/posts/me", {
+    headers: headers,
+  });
   return response.data;
 };
