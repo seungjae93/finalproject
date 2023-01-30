212e1d1bc1d352ab60b31f58edd20c603de72df6
heeseung lee <heeseung721@gmail.com>
2023-01-30 22:09:27 +0900

    :art:fix:reviewApi 간단 수정

--- src/redux/api/reviewApi.js
+++ src/redux/api/reviewApi.js
@@ -2,19 +2,29 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { instance } from "./instance";
 import setToken from "../../shared/setToken";
 import { getCookie } from "../../shared/cookie";
-import { checkLogin } from "../../hooks/useCheckLogin";
+// import { checkLogin } from "../../hooks/useCheckLogin";
 
-const { accessToken, isLogin } = checkLogin();
+// const { accessToken, isLogin } = checkLogin();
 
 // GET
 export const getPosts = async () => {
-  const response = await instance.get("/review");
+  const accessToken = getCookie("token");
+  setToken(accessToken);
+  const headers = { authorization: `Bearer ${accessToken}` };
+  const response = await instance.get("/review", {
+    headers: headers,
+  });
   return response.data;
 };
 
 // POST
 export const addPost = async (post) => {
-  const response = await instance.post("/review", post);
+  const accessToken = getCookie("token");
+  setToken(accessToken);
+  const headers = { authorization: `Bearer ${accessToken}` };
+  const response = await instance.post("/review", post, {
+    headers: headers,
+  });
   return response.data;
 };
 
