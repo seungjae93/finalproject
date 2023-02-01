93f9abbe59f87f1d96a4210267dfa30b35cd2889
jominuk <jominuk1025@naver.com>
2023-02-02 00:31:07 +0900

    :ambulance: fix:로그인 상태관리 수정

--- src/pages/Comment.jsx
+++ src/pages/Comment.jsx
@@ -24,7 +24,8 @@ const Comment = () => {
 
   const deleteCommentCallback = async (commentId) => {
     await deleteComment(commentId);
-    queryClient.invalidateQueries(["comments", postId]);
+    alert("삭제가 완료되었습니다.");
+    queryClient.invalidateQueries(["comments"]);
   };
 
   const { mutate } = useMutation(
