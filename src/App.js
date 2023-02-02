41d76358fbd3b422df9bc4fb39e8dcf585d967d7
seungjae93 <lsj72023892@gmail.com>
2023-02-02 21:55:32 +0900

    :recycle: feat: 코드 수정 및 cookie 파일 삭제

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import Router from "./shared/Router";
 import GlobalStyle from "./components/styles/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useDispatch } from "react-redux";
-import { getCookie } from "./shared/cookie";
-import { loginCheck } from "./redux/modules/kakaoSlice";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
@@ -12,12 +9,6 @@ const queryClient = new QueryClient({
 });
 
 function App() {
-  const dispatch = useDispatch();
-  const accessToken = getCookie("token");
-
-  if (accessToken) {
-    dispatch(loginCheck(true));
-  }
   return (
     <>
       <QueryClientProvider client={queryClient}>
