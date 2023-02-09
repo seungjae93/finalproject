55c13455f37eaac5c8c0540715a9d51437120419
seungjae93 <lsj72023892@gmail.com>
2023-02-09 21:52:36 +0900

    :recycle: refactor: vercel 오류 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from "../pages/mainpage/MainPage";
+import MainPage from "../pages/MainPage";
 import Header from "../shared/Header";
 import Footer from "../shared/Footer";
 import ScrollToTop from "../shared/ScrollToTop";
-import MainMap from "../pages/map/MainMap";
-import KakaoLogin from "../pages/login/KakaoLogin";
-import Login from "../pages/login/Login";
-import ReviewPage from "../pages/reviewpage/ReviewPage";
-import Mypage from "../pages/mypage/Mypage";
-import PostList from "../pages/community/PostList";
-import PostDetail from "../pages/community/PostDetail";
-import PostEdit from "../pages/community/PostEdit";
+import MainMap from "../pages/MainMap";
+import KakaoLogin from "../pages/KakaoLogin";
+import Login from "../pages/Login";
+import ReviewPage from "../pages/ReviewPage";
+import Mypage from "../pages/Mypage";
+import PostList from "../pages/PostList";
+import PostDetail from "../pages/PostDetail";
+import PostEdit from "../pages/PostEdit";
 import PrivateRoutes from "./PrivateRoutes";
 
 const Router = () => {
