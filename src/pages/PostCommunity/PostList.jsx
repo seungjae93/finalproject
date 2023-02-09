a20c1d09fa2a03fb78c69aa9aaf41977678ddabb
seungjae93 <lsj72023892@gmail.com>
2023-02-09 23:21:36 +0900

    :recycle: refactor: import 수정

--- src/pages/PostCommunity/PostList.jsx
+++ src/pages/PostCommunity/PostList.jsx
@@ -8,7 +8,7 @@ import { debounce } from "lodash";
 import { useInView } from "react-intersection-observer";
 import { getCommunity } from "../../redux/api/communityApi";
 import InfiniteScroll from "react-infinite-scroller";
-import { SeletedOne } from "../components/Community/Selected";
+import { SeletedOne } from "../../components/Community/Selected";
 import Button from "../components/button/Button";
 
 const PostList = () => {
